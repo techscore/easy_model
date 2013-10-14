948aa8413a1c3cb1b430b5955085b7ecbe960575
SUZUKI Kei <todokimasen@gmail.com>
2013-10-14 22:39:04 +0900

    EasyModel::Base is deprecated now.

--- lib/easy_model/base.rb
+++ lib/easy_model/base.rb
@@ -4,5 +4,9 @@ class EasyModel::Base
 
   include EasyModel::Column
 
+  def self.inherited(base)
+    $stderr.puts 'DEPRECATED: EasyModel::Base is deprecated. Instead to include EasyModel::Column.'
+  end
+
 end
 
